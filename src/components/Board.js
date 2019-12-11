49d4a2c8a721c4c5cba74296116fc2b93f006da6
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2019-12-11 17:58:43 -0500

    Removed row class in favor of using CSS grid to handle rows and columns

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import Row from './Row';
+import Cell from './Cell';
 
 class Board extends React.Component {
-  getStyle(height) {
+  getStyle(height, width) {
     const style = {
       display: 'grid',
-      gridTemplateRows: `repeat(${height}, 1fr)`
+      gridTemplateRows: `repeat(${height}, 50px)`,
+      gridTemplateColumns: `repeat(${width}, 50px)`
     }
 
     return style;
   }
 
   renderBoard(height, width) {
-    return Array(height).fill(0).map(() => <Row width={width} />);
+    return Array(height * width).fill(0).map(() => <Cell />);
   }
   
   render () {
     return (
-      <div style={this.getStyle(this.props.height)}>
+      <div style={this.getStyle(this.props.height, this.props.width)}>
         { this.renderBoard(this.props.height, this.props.width) }
       </div>
     );
