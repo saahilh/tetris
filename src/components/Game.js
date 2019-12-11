6eea300e18e977ad89ece0a105d8a844fdbc57b9
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2019-12-11 13:35:17 -0500

    Created board component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import Timer from './Timer'
+import Timer from './Timer';
+import Board from './Board';
 
 class Game extends React.Component {
-    render () {
-        return (
-            <div className="game">
-                <Timer />
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div className="game">
+        <Timer />
+        <Board />
+      </div>
+    );
+  }
 }
 
 export default Game;
