d7d54660e26b4a6b6a0580feeb747e5c6db77fa6
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2019-12-11 18:32:11 -0500

    Created block class

---
+++ src/components/Block.js
@@ -0,0 +1,17 @@
+class Block {
+  constructor() {
+    this.x = -1;
+    this.y = -1;
+    this.color = 'none';
+  }
+
+  static getRandomBlock() {
+    return '';
+  }
+
+  getShape() {
+    return 'undefined';
+  }
+}
+
+export default Block;
