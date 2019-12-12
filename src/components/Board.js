8c5986b865e36ab7a45236a2b3c3aeaabc8fe3cf
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2019-12-11 23:12:53 -0500

    Started tick logic for game to update, moved lots of logic from game to board as I try to figure out where it belongs, wrote update method for board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,21 +1,54 @@
 import React from 'react';
 import Cell from './Cell';
+import Block from './Block';
 
 class Board extends React.Component {
+  getTickRate = () => 1;
+
   getStyle = (boardDimensions) => ({
     display: 'grid',
     gridTemplateRows: `repeat(${boardDimensions.height}, 25px)`,
     gridTemplateColumns: `repeat(${boardDimensions.width}, 25px)`
   });
 
+  getBoardDimensions = () => ({
+    height: 20,
+    width: 10
+  });
+
+  getBlockStartCoordinates = () => ({
+    x: 0,
+    y: this.boardWidth/2 - 1
+  });
+
   renderBoard = (boardDimensions) => (
     Array(boardDimensions.height * boardDimensions.width).fill(0).map(() => <Cell />)
   );
+
+  setNewBlock() {
+    this.setState({
+      currentBlock: Block.getRandomBlock(this.getBlockStartCoordinates())
+    });
+  }
+
+  componentDidMount() {
+    this.setState({
+      ticker: setInterval(() => this.update(), this.getTickRate() * 1000)
+    })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.ticker);
+  }
+
+  update() {
+
+  }
   
   render () {
     return (
-      <div style={this.getStyle(this.props.boardDimensions)}>
-        { this.renderBoard(this.props.boardDimensions) }
+      <div style={this.getStyle(this.getBoardDimensions())}>
+        { this.renderBoard(this.getBoardDimensions()) }
       </div>
     );
   }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,52 +1,20 @@
 import React from 'react';
 import Timer from './Timer';
 import Board from './Board';
-import Block from './Block';
 
 class Game extends React.Component {
-  getTickRate = () => 1;
-
-  getBoardDimensions = () => ({
-    height: 20,
-    width: 10
-  });
-
-  getBlockStartCoordinates = () => ({
-    x: 0,
-    y: this.boardWidth/2 - 1
-  });
-
   getStyle = () => ({
     display: 'table',
     margin: '0 auto'
   });
 
-  setNewBlock() {
-    this.setState({
-      currentBlock: Block.getRandomBlock(this.getBlockStartCoordinates())
-    });
-  }
-
-  componentWillMount(){
-    this.setNewBlock();
-  }
-
-  renderGame() {
+  render () {
     return(
       <div style={this.getStyle()}>
         <Timer />
-        <Board boardDimensions={this.getBoardDimensions()} />
+        <Board />
       </div>
     );
-  }
-
-  render () {
-    if(this.state.currentBlock.hasCollided()){
-      this.setNewBlock();
-    }
-    else{
-      return this.renderGame();
-    }
   }
 }
 
