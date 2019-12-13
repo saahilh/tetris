78fc74ae806c1f0e21f329080b99f17c424f5454
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2019-12-12 23:27:17 -0500

    Flipped x and y for start poisition as they were backwards

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const BOARD_HEIGHT_CELLS = 20;
 export const BOARD_WIDTH_CELLS = 10;
 export const BLOCK_START_COORDINATES = {
-  x: 0,
-  y: BOARD_WIDTH_CELLS/2 - 1
+  x: BOARD_WIDTH_CELLS/2 - 1,
+  y: 0
 }
 export const BLOCK_TYPES = ['I'];
