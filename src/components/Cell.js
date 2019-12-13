dec26e261c37892488e536514d0af674fe526c26
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2019-12-13 00:42:04 -0500

    Removed width and height properties from cell to fix graphical bug

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 class Cell extends React.Component {
   getStyle = (cellColor) => ({
-    width: '100%',
-    height: '50px',
     border: 'solid 1px black',
     backgroundColor: cellColor
   });
