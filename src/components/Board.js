f6c503164c1faf2057065e7e2e911908b3183a75
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2019-12-13 00:19:04 -0500

    Fixed cell list generation as current implementation using fill was only referencing a single object in all rows

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,12 +4,26 @@ import Row from './Row';
 import * as C from '../constants';
 
 class Board extends React.Component {
+  generateBoardCells() {
+    let cells = Array(C.BOARD_HEIGHT_CELLS);
+
+    for(let i = 0; i < C.BOARD_HEIGHT_CELLS; i++){
+      cells[i] = Array(C.BOARD_WIDTH_CELLS).fill('white');
+    }
+
+    return cells;
+  }
+
   componentWillMount() {
     this.setState({
       ticker: setInterval(() => this.update(), 1000),
       currentBlock: Block.getRandomBlock(),
-      cellColors: Array(C.BOARD_HEIGHT_CELLS).fill(Array(C.BOARD_WIDTH_CELLS).fill('white'))
-    })
+      cellColors: this.generateBoardCells()
+    });
+  }
+
+  componentDidMount() {
+    this.update();
   }
 
   componentWillUnmount() {
@@ -18,17 +32,29 @@ class Board extends React.Component {
 
   storeCells(block){
     let currentCells = this.state.cellColors;
+    let currentBlock = this.state.currentBlock;
 
+    let shape = currentBlock.getShape();
+
+    let x = currentBlock.getX();
+    let y = currentBlock.getY();
+    
+    for(let i = y; i < y + shape.length; i++){
+      for(let j = x; j < x + shape[0].length; j++){
+        currentCells[i][j] = currentBlock.getColor();
+      }
+    }
+    
     this.setState({
       cellColors: currentCells
     });
   }
 
   update() {
+    this.storeCells(this.state.currentBlock);
+
     let hasCollided = this.state.currentBlock.moveDown();
 
-    this.storeCells(this.state.currentBlock);
-    
     if(hasCollided){
       this.setState({
         currentBlock: Block.getRandomBlock()
@@ -42,9 +68,9 @@ class Board extends React.Component {
     gridTemplateColumns: `repeat(${C.BOARD_WIDTH_CELLS}, 25px)`
   });
 
-  renderRows = () => (
-    this.state.cellColors.map((cellColorList) => <Row cellColorList={cellColorList} />)
-  );
+  renderRows = () => {
+    return this.state.cellColors.map((cellColorList) => <Row cellColorList={cellColorList} />)
+  };
 
   render () {
     return (
