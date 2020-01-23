9010618074a492ebb82f4931e0fc78f3ca679d37
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2020-01-22 22:20:29 -0500

    Fix for addScore logic

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,33 +3,35 @@ import GameInfo from './GameInfo';
 import Timer from './Timer';
 import Board from './Board';
 
+const getStyle = () => ({
+  display: 'grid',
+  margin: '50px',
+  gridTemplateAreas: 
+      `"left-gutter board timer right-gutter"
+      "left-gutter board score right-gutter"
+      "left-gutter board space right-gutter"
+      "left-gutter board space right-gutter"
+      `
+});
+
 function Game(props) {
   const {restartGame} = props;
   const [score, setScore] = useState(0);
-
-  const addScore = () => setScore(prev => prev + 1);
 
   const gameOver = () => {
     alert(`Game over! Score was ${score}`)
     props.restartGame();
   }
 
-  const getStyle = () => ({
-    display: 'grid',
-    margin: '50px',
-    gridTemplateAreas: 
-        `"left-gutter board timer right-gutter"
-        "left-gutter board score right-gutter"
-        "left-gutter board space right-gutter"
-        "left-gutter board space right-gutter"
-        `
-  });
-
   return(
     <div style={getStyle()}>
       <Timer startTime={Math.floor(Date.now() / 1000)} />
       <GameInfo gridArea="score" heading="Score" info={`${score}`} />
-      <Board addScore={addScore} restartGame={restartGame} gameOver={gameOver}/>
+      <Board 
+        addScore={(score) => setScore(prev => prev + score)} 
+        restartGame={restartGame} 
+        gameOver={gameOver}
+      />
     </div>
   );
 }
