99b784cdba6a05aee94ffeeecbbfe488dd7a870d
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2020-01-22 21:57:16 -0500

    Made Tetris into a functional component to simplify logic

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,30 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Game from './Game';
 
-class Tetris extends React.Component {
-  constructor() {
-    // Used to set key for game to create a new game
-    // instance when updated; a bit of a weird way
-    // to do it but the only way I can find to do so
-    // using React right now.
-    super()
+function Tetris(props) {
+  const [newGame, setnewGame] = useState(false);
 
-    this.state = {
-      gameInstanceNumber: 0
-    }
-  }
-
-  restartGame = () => {
-    this.setState({
-      gameInstanceNumber: this.state.gameInstanceNumber + 1
-    });
-  }
-
-  render () {
-    return(
-      <Game key={this.state.gameInstanceNumber} restartGame={this.restartGame} />
-    );
-  }
+  return(
+    <Game key={newGame} restartGame={() => setnewGame(prev => !prev)} />
+  );
 }
 
 export default Tetris;
