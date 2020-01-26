7660e9f0f107ee9f36e3538955a6c19249f8910d
Saahil Hamayun <saahil.hamayun@mail.mcgill.ca>
2020-01-26 01:39:44 -0500

    Updated indentation of GameInfo to 2

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 const getStyle = gridArea => ({
-    fontSize: '2em',
-    padding: '10px',
-    width: '100%',
-    border: 'solid black 1px',
-    gridArea: gridArea
+  fontSize: '2em',
+  padding: '10px',
+  width: '100%',
+  border: 'solid black 1px',
+  gridArea: gridArea
 });
 
 function GameInfo(props) {
-    const {gridArea, heading, info} = props;
+  const {gridArea, heading, info} = props;
 
-    return(
-        <div style={getStyle(gridArea)}>
-            <span>{heading}: </span>
-            <span>{info}</span>
-        </div>
-    );
+  return(
+    <div style={getStyle(gridArea)}>
+      <span>{heading}: </span>
+      <span>{info}</span>
+    </div>
+  );
 }
 
 export default GameInfo;
