c893463950c5f83ab9b734982a6aada4fc083247
Saahil Hamayun <saahilhamayun@hotmail.com>
2020-08-16 03:44:06 +0400

    Added a proper landing screen and game over screen

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,18 +6,24 @@ import BlockDisplayPanel from './BlockDisplayPanel';
 
 function Game() {
   const [score, setScore] = useState(0);
-  const [newGame, setNewGame] = useState(false);
   const [nextBlock, setNextBlock] = useState(null);
   const [savedBlock, setSavedBlock] = useState(null);
 
+  const [initialLoad, setInitialLoad] = useState(true);
+  const [gameEnded, setGameEnded] = useState(true);
+
+  const startFirstGame = () => {
+    setInitialLoad(false);
+    setGameEnded(false);
+  }
+
   const restartGame = () => {
     setScore(0);
-    setNewGame(prev => !prev);
+    setGameEnded(false);
   };
 
   const gameOver = () => {
-    alert(`Game over! Score was ${score}`);
-    restartGame();
+    setGameEnded(true);
   };
 
   const getGameContainerStyle = () => ({
@@ -36,6 +42,15 @@ function Game() {
     margin: 'auto',
   });
 
+  const getGameOverStyle = () => ({
+    width: 'max-content',
+    padding: '25px 50px',
+    borderRadius: '5px',
+    backgroundColor: 'white',
+    margin: 'auto',
+    textAlign: 'center',
+  });
+
   const updateNextBlock = (nextBlock) => {
     setNextBlock(nextBlock);
   };
@@ -44,11 +59,36 @@ function Game() {
     setSavedBlock(savedBlock);
   };
 
-  return(
+  if (initialLoad) {
+    return (
+      <div style={getGameContainerStyle()}>
+        <h1 style={{textAlign: 'center', color: 'white', fontSize: 80, margin: 0, fontFamily: 'Bungee Shade'}}>TETRIS</h1>
+        <div style={getGameOverStyle()}>
+          <h2 style={{fontFamily: 'Bungee Shade'}}>Welcome to Tetris RH</h2>
+          <button onClick={startFirstGame}>Start Game</button>
+        </div>
+      </div>
+    );
+  }
+
+  if (gameEnded) {
+    return (
+      <div style={getGameContainerStyle()}>
+        <h1 style={{textAlign: 'center', color: 'white', fontSize: 80, margin: 0, fontFamily: 'Bungee Shade'}}>TETRIS</h1>
+        <div style={getGameOverStyle()}>
+          <h2 style={{fontFamily: 'Bungee Shade'}}>Game Over</h2>
+          <h3 style={{fontFamily: 'Graduate'}}>Score was {score}</h3>
+          <button onClick={restartGame}>Restart</button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
     <div style={getGameContainerStyle()}>
       <h1 style={{textAlign: 'center', color: 'white', fontSize: 80, margin: 0, fontFamily: 'Bungee Shade'}}>TETRIS</h1>
 
-      <div key={newGame} style={getGameStyle()}>
+      <div style={getGameStyle()}>
         <Board 
           updateNextBlock={updateNextBlock}
           updateSavedBlock={updateSavedBlock}
