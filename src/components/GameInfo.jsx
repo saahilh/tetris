23cf2310c97e73c42a44f413766e5b7756fecd25
Saahil Hamayun <saahilhamayun@hotmail.com>
2020-08-15 22:46:46 +0400

    Added 0 padding to GameInfo to make it a bit prettier

--- src/components/GameInfo.jsx
+++ src/components/GameInfo.jsx
@@ -8,14 +8,21 @@ const getStyle = () => ({
   border: 'solid black 1px',
 });
 
-function GameInfo({heading, info}) {
+const GameInfo = ({heading, info}) => {
+  const leftPadZeros = (numberAsString, digitsRequired) => {
+    const numberOfZerosToPrepend = digitsRequired - numberAsString.length;
+    const updatedNumberAsString = '0'.repeat(numberOfZerosToPrepend) + numberAsString;
+    return updatedNumberAsString;
+  };
+
   return(
     <div style={getStyle()}>
       <span style={{width: '100%'}}>
-        {heading}: {info}
+        <span style={{float: 'left'}}>{heading}:</span>
+        <span style={{float: 'right'}}>{leftPadZeros(info, 5)}</span>
       </span>
     </div>
   );
-}
+};
 
 export default GameInfo;
